701c88c0ddb98042daecae9256ff3e94c44248f3
Nicola <De Lazzari>
2021-02-01 09:16:32 +0100

    add template and scss

--- giovannipante.php
+++ giovannipante.php
@@ -6,42 +6,62 @@
 get_header('giovannipante'); // This fxn gets the header.php file and renders it ?>
 	<section>
 		<div id="content" role="main">
-            <div>
-                <?php
-                    $args = array( 'post_type'=>'sezione');
-                    $loop = new WP_Query( $args );
+            <?php
+                $args = array( 'post_type'=>'sezione');
+                $loop = new WP_Query( $args );
 
-                    // Start the loop for your custom query
-                    if($loop->have_posts() ) : while ($loop->have_posts() ) : $loop->the_post();
+                // Start the loop for your custom query
+                if($loop->have_posts() ) : while ($loop->have_posts() ) : $loop->the_post();
 
-                    the_title();
-                    the_content();
-
-                    endwhile;
-                    endif;
                 ?>
-            </div>
-
-            <div class="flex-grid">
+                <div class="section">
+                    <h2 class="title">
+                        <?php the_title(); ?>
+                    </h2>
+                    <div class="text">
+                        <?php the_content(); ?>
+                    </div>
+                </div>
                 <?php
-                    $args = array( 'post_type'=>'progetto');
-                    $loop = new WP_Query( $args );
 
-                    // Start the loop for your custom query
-                    if($loop->have_posts() ) : while ($loop->have_posts() ) : $loop->the_post();
-                ?>
-                <div class="col">
+                endwhile;
+                endif;
+            ?>
+
+	        <div class="section">
+	            <div class="title">
+                    <h2>Selected works</h2>
+                </div>
+                <div class="flex-grid">
                     <?php
-                        the_title();
-                        the_content();
+                        $args = array( 'post_type'=>'progetto');
+                        $loop = new WP_Query( $args );
+
+                        // Start the loop for your custom query
+                        if($loop->have_posts() ) : while ($loop->have_posts() ) : $loop->the_post();
                     ?>
+
+                    <div class="col">
+                        <div class="project">
+                            <h3 class="title">
+                                <?php the_title(); ?>
+                            </h2>
+                            <div class="thumb">
+                                <? if( has_post_thumbnail( $post_id ) ): ?>
+                                    <img title="image title" alt="thumb image" class="wp-post-image" src="<?=wp_get_attachment_url( get_post_thumbnail_id() ); ?>">
+                                <? endif; ?>
+                            </div>
+                            <div class="text">
+                                <?php the_content(); ?>
+                            </div>
+                        </div>
                     </div>
                     <?php
-
-                    endwhile;
-                    endif;
-                ?>
-            </div>
+                        endwhile;
+                        endif;
+                    ?>
+                </div>
+	        </div>
 		</div><!-- #content .site-content -->
 	</section><!-- #primary .content-area -->
 <?php get_footer('giovannipante'); // This fxn gets the footer.php file and renders it ?>
--- header-giovannipante.php
+++ header-giovannipante.php
@@ -44,23 +44,10 @@
 >
 
 <header id="masthead" class="site-header">
-	<div class="container center">
-
-		<nav class="site-navigation main-navigation">
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); // Display the user-defined menu in Appearance > Menus ?>
-		</nav><!-- .site-navigation .main-navigation -->
-	</div>
-	<div class="center">
-
-		<div id="brand">
-			<h1 class="site-title">
-				<a href="<?php echo esc_url( home_url( '/' ) ); // Link to the home page ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); // Title it with the blog name ?>" rel="home"><?php bloginfo( 'name' ); // Display the blog name ?></a>
-			</h1>
-		</div><!-- /brand -->
-
-		<div class="clear"></div>
-	</div><!--/container -->
-
+	<h1 class="site-title">
+        <a href="<?php echo esc_url( home_url( '/' ) ); // Link to the home page ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); // Title it with the blog name ?>" rel="home"><?php bloginfo( 'name' ); // Display the blog name ?></a>
+    </h1>
+    <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); // Display the user-defined menu in Appearance > Menus ?>
 </header><!-- #masthead .site-header -->
 
 <main class="main-fluid"><!-- start the page containter -->
