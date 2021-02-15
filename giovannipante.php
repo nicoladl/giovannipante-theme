8a562522d3b8bb1f8ad82e2174093d4add17da65
Nicola De Lazzari <nicola.delazzari@movedigital.ch>
2021-02-15 22:22:52 +0100

    feat add contacts section

--- giovannipante.php
+++ giovannipante.php
@@ -73,43 +73,54 @@ get_header('giovannipante'); // This fxn gets the header.php file and renders it
 	            <div class="title">
                     <h2>Contacts</h2>
                 </div>
-	            <?php
-                    $args = array( 'post_type'=>'social');
-                    $loop = new WP_Query( $args );
+                <div class="flex-grid">
+                    <div class="col">
+                        <ul class="socials">
+                            <?php
+                                $args = array( 'post_type'=>'social');
+                                $loop = new WP_Query( $args );
 
-                    // Start the loop for your custom query
-                    if($loop->have_posts() ) : while ($loop->have_posts() ) : $loop->the_post();
+                                // Start the loop for your custom query
+                                if($loop->have_posts() ) : while ($loop->have_posts() ) : $loop->the_post();
 
-                    ?>
-                    <div>
-                        <a href="<?php echo wp_strip_all_tags( get_the_content() ); ?>" target="_blank">
-                            <?php the_title(); ?>
-                        </a>
+                                ?>
+                                <li>
+                                    <a href="<?php echo wp_strip_all_tags( get_the_content() ); ?>" target="_blank">
+                                        <?php the_title(); ?>
+                                    </a>
+                                </li>
+                                <?php
+
+                                endwhile;
+                                endif;
+                            ?>
+                        </ul>
                     </div>
-                    <?php
 
-                    endwhile;
-                    endif;
-                ?>
+                    <div class="col">
+                        <ul class="contacts">
+                            <?php
+                                $args = array( 'post_type'=>'contatti');
+                                $loop = new WP_Query( $args );
 
-                <?php
-                    $args = array( 'post_type'=>'contatti');
-                    $loop = new WP_Query( $args );
+                                // Start the loop for your custom query
+                                if($loop->have_posts() ) : while ($loop->have_posts() ) : $loop->the_post();
 
-                    // Start the loop for your custom query
-                    if($loop->have_posts() ) : while ($loop->have_posts() ) : $loop->the_post();
+                                ?>
+                                <li>
+                                    <p><?php the_field('title'); ?></p>
+                                    <a href="<?php echo wp_strip_all_tags( get_the_content() ); ?>" target="_blank">
+                                        <?php the_field('link_label'); ?>
+                                    </a>
+                                </li>
+                                <?php
 
-                    ?>
-                    <div>
-                        <a href="<?php echo wp_strip_all_tags( get_the_content() ); ?>" target="_blank">
-                            <?php the_title(); ?>
-                        </a>
+                                endwhile;
+                                endif;
+                            ?>
+                        </ul>
                     </div>
-                    <?php
-
-                    endwhile;
-                    endif;
-                ?>
+                </div>
 	        </div>
 		</div><!-- #content .site-content -->
 	</section><!-- #primary .content-area -->
